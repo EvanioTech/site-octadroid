f1c1f6ae00e57c4084268c94f3d46cfa3114084b
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2023-12-21 17:04:57 -0300

    home finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ServicosComponent } from './pages/servicos/servicos.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatosComponent } from './pages/contatos/contatos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:HomeComponent},
+  { path: 'servicos' , component: ServicosComponent},
+  {path: 'contatos', component: ContatosComponent}
+];
